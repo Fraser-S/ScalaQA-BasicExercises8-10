822d146134be869148117c80175b8051dc588546
Fraser <frasers@hotmail.co.uk>
2017-06-05 14:07:33 +0000

    Created the main function. Added an addition function that adds two numbers together

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,22 @@
   */
 object Main {
 
-  def addition(num1 : Int, num2 : Int) : Int= {
-    num1 + num2
+  //adds two value together if add is true,if false will multiply them together
+  def addition(num1 : Int, num2 : Int, add : Boolean) : Int= {
+
+    var num3 : Int = 0
+
+    if(add == true){
+      num3 = num1 + num2
+    } else {
+      num3 = num1 * num2
+    }
+    num3
   }
 
   def main(args : Array[String]) : Unit = {
-    println(addition(2,5))
+    println("2 + 5 = " + addition(2,5, true))
+    println("2 * 5 = " + addition(2,5,false))
+
   }
 }
