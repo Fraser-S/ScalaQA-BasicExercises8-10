20920ba291ed91ed83be9814caf626a1844ad40c
Fraser <frasers@hotmail.co.uk>
2017-06-05 14:15:41 +0000

    Added a check that returns a the non if a zero is passed in, unless both are zero

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,12 +3,17 @@
   */
 object Main {
 
-  //adds two value together if add is true,if false will multiply them together
+  //adds two value together if add is true,if false will multiply them together. will return the larger number if um1 or num2 is 0
   def addition(num1 : Int, num2 : Int, add : Boolean) : Int= {
 
     var num3 : Int = 0
 
-    if(add){
+    if(num1 == 0 || num2 == 0){
+      if(num1 != 0)
+        num3 = num1
+      else
+        num3 = num2
+    } else if(add){
       //if add is true add the numbers
       num3 = num1 + num2
     } else {
@@ -21,6 +26,7 @@ object Main {
   def main(args : Array[String]) : Unit = {
     println("2 + 5 = " + addition(2,5, true))
     println("2 * 5 = " + addition(2,5,false))
-
+    println("0,5 passed: " + addition(0,5,false))
+    println("2,0 passed: " + addition(2,0,true))
   }
 }
