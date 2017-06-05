0031756cf1225ac8761dc601c8cab949846ae9de
Fraser <frasers@hotmail.co.uk>
2017-06-05 14:01:29 +0000

    Created the main function. Added an addition function that adds two numbers together

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by Fraser on 05/06/2017.
+  */
+object Main {
+
+  def addition(num1 : Int, num2 : Int) : Int= {
+    num1 + num2
+  }
+
+  def main(args : Array[String]) : Unit = {
+    println(addition(2,5))
+  }
+
+}
